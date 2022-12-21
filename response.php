4671a30cbfd172b812d05b97ac7c6dae0d65c823
shciafffs <111536843+shciafffs@users.noreply.github.com>
2022-12-21 16:19:53 -0500

    v1

---
+++ response.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    <?php
+            $server="localhost";
+            $username="alessio";
+            $password="Password01";
+            $database="final";
+            $conn=mysqli_connect($server, $username, $password, $database);
+
+            $searchterm=htmlspecialchars($_POST['searchbar']);
+            
+
+
+            // $sql="INSERT INTO logs (FNAME, LNAME, SEX, USERNAME, PASSWORD, CONDITIONS) values('$fname', '$lname', $sex, '$username', '$password', '$conditions');";
+            // $result=mysqli_query($conn, $sql);
+            // if ($result) {
+            //     echo "New record created successfully";
+            // } else {
+            //     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+            // }
+
+            mysqli_close($conn);
+    ?>
+    </head>
+    <body>
+        <!-- <p>Your First Name is: <?= htmlspecialchars($_POST['FNAME']) ?></p><br>
+        <p>Your Last Name is: <?= htmlspecialchars($_POST['LNAME']) ?></p><br>
+        <p>Your UserName is: <?= htmlspecialchars($_POST['USERNAME']) ?></p><br>
+        <p>Your Password is: <?= htmlspecialchars($_POST['PASSWORD']) ?></p><br>
+        <p>Your Gender is: <?= $sex ?></p><br>
+        <p>Your Conditions are: <?= htmlspecialchars($_POST['CONDITIONS']) ?></p><br> -->
+        <?= echo $searchterm ?>
+    </body>
+</html>
